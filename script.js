9953ed7b8709731e58675120bf50be76ab04ead6
Aden Teo <adenteo@gmail.com>
2023-01-10 12:44:10 +0800

    Added bootstrap

--- script.js
+++ script.js
@@ -5,23 +5,8 @@ function toggleMenu() {
 
 $(document).ready(function () {
     // Activate Carousel
-    $(".carousel").carousel({ interval: 2000 });
+    $(".carousel").carousel({ interval: false });
 
-    // Enable Carousel Indicators
-    // $(".item1").click(function () {
-    //     $("#carousel-slide").carousel(0);
-    // });
-    // $(".item2").click(function () {
-    //     $("#carousel-slide").carousel(1);
-    // });
-    // $(".item3").click(function () {
-    //     $("#carousel-slide").carousel(2);
-    // });
-    // $(".item4").click(function () {
-    //     $("#carousel-slide").carousel(3);
-    // });
-
-    // Enable Carousel Controls
     $(".left").click(function () {
         $(".carousel").carousel("prev");
     });
